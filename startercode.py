d2c6793d3831ebd320f6857cb9dcc42afd62c988
Tracy Ni <tracyni@TracydeMacBook-Pro-1252.local>
2026-03-31 16:21:42 -0400

    finished the fourth function

--- startercode.py
+++ startercode.py
@@ -111,7 +111,29 @@ def update_cache(breed_ids, cache_file):
         A string: "Cached data for {percentage}% of breeds",
         where percentage = (successful_new_adds / len(breed_ids)) * 100.
     """
-    pass
+    cache = load_json(cache_file)
+    successful_new_adds = 0
+
+    for breed_id in breed_ids:
+        url = f"https://dogapi.dog/api/v2/breeds/{breed_id}"
+
+        if url in cache:
+            continue
+
+        result = search_breed(breed_id)
+        if result is not None:
+            parsed_json, response_url = result
+            cache[response_url] = parsed_json
+            successful_new_adds += 1
+
+    create_cache(cache, cache_file)
+
+    if len(breed_ids) == 0:
+        percentage = 0.0
+    else:
+        percentage = (successful_new_adds / len(breed_ids)) * 100
+
+    return f"Cached data for {percentage}% of breeds"
 
 
 def get_longest_lifespan_breed(cache_file):
